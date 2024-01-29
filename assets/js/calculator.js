b7477a9341d2c0a0e3e6306f9d19bdf84a6fd28c
felipe <gallegofelipe29@gmail.com>
2024-01-29 17:04:01 -0500

    calculator

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,5 +1,3 @@
-
-
 const informationCredit = document.getElementById('valueSee')
 const rangeCredit = document.getElementById('range')
 const totalCredit = document.getElementById('totalMonto') 
@@ -7,13 +5,14 @@ const totalCuotas = document.getElementById('totalCuotas')
 const btnSimularCredito = document.getElementById('btnSimularCredito')
 const valorCuota = document.getElementById('valorCuota')
 const totalIntereses = document.getElementById('totalIntereses')
+const totalPagarCredito = document.getElementById('totalPagar')
 const porcentajeIntereses = 0.0429  
 
 
-
+const formCalculator = document.getElementById('formCalculator')
 //SEE CUOTAS CREDIT
-//totalCuotas.innerHTML = document.cuotas.value
-  rangeCredit.addEventListener('input', function () {
+
+rangeCredit.addEventListener('input', function () {
 
     let COP = new Intl.NumberFormat('en-US', {
         style: 'currency',
@@ -22,9 +21,9 @@ const porcentajeIntereses = 0.0429
 
     informationCredit.textContent = COP.format(rangeCredit.value);
     totalCredit.textContent = COP.format(rangeCredit.value)
-    }) 
+}) 
 
-function simularCredito() {
+formCalculator.addEventListener('change', function simularCredito() {
 
     //mostrar cuotas
     let cuotasCheckbox = document.querySelector('input[name=cuotasCredito]:checked').value;
@@ -35,14 +34,14 @@ function simularCredito() {
     style: 'currency',
     currency: 'COP',    
     });
-    let valorIntereses = parseFloat(rangeCredit.value) * porcentajeIntereses
+    let valorIntereses = parseFloat(rangeCredit.value) * porcentajeIntereses * cuotasCheckbox
     let valorCuotaCredito = (valorIntereses + parseFloat(rangeCredit.value)) / cuotasCheckbox
-    valorCuota.textContent = COP.format(valorCuotaCredito)
+    let totalPagarCredito = parseFloat(rangeCredit.value) + valorIntereses
     
-    //VALOR INTERESES
-    //if (cuotasCheckbox === '1')
+    valorCuota.textContent = COP.format(valorCuotaCredito)
     totalIntereses.innerText = COP.format (valorIntereses)
-}
+    totalPagar.textContent = COP.format(totalPagarCredito)
+}) 
 
-btnSimularCredito.onclick = simularCredito
+
 
