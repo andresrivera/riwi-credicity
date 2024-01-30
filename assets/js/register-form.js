b47fd0e5678ddf0b1e59a77ffc97d8e9868e42ac
felipe <gallegofelipe29@gmail.com>
2024-01-30 15:47:38 -0500

    create new function add  user

--- assets/js/register-form.js
+++ assets/js/register-form.js
@@ -1,66 +1,55 @@
+//================================== CREATE NEW USER =========================================
+const tiempoTranscurrido = Date.now();
+const hoy = new Date(tiempoTranscurrido);
+hoy.toLocaleDateString();
+console.log(hoy);
 
-function mostrar(){
-    let name = document.getElementById("name").value;
+//let termAndConditions = document.getElementById("termAndConditions");
+//let personalData = document.getElementById("personalData");
+
+let btnRegister = document.getElementById('btnRegister');
+
+function addNewUser (){
+
+    let nameUser = document.getElementById("name").value;
     let last_name = document.getElementById("last_name").value;
+    let genderUser = document.querySelector('input[name="gendern"]:checked').value;
     let type_document = document.getElementById("type_document").value;
     let document_number = document.getElementById("document").value;
     let document_expedition = document.getElementById("document_expedition").value;
-    let birthday = document.getElementById("birthday").value;
-    
-    
-    let gendern = document.querySelector('input[name="gendern"]:checked').value;
-    
-    let phone_number = document.getElementById("phone_number").value;
-    let email = document.getElementById("email").value;
-    
-    let country = document.getElementById("country").value;
-    let state = document.getElementById("state").value;
-    let city = document.getElementById("city").value;
-    
+    let birthdayUser = document.getElementById("birthday").value;
+    let emailUser = document.getElementById("email").value;
+    let phoneNumberUser = document.getElementById("phone_number").value;
+    let countryUser = document.getElementById("country").value;
+    let stateUser = document.getElementById("state").value;
+    let cityUser = document.getElementById("city").value;
     let create_password = document.getElementById("create_password").value;
-    let check_password = document.getElementById("check_password").value;
-    
-    
-    let termAndConditions = document.getElementById("termAndConditions").value;
-    let personalData = document.getElementById("personalData").value;
-    
-    console.log(name)
-    console.log(last_name);
-    console.log(type_document);
-    console.log(document_number);
-    console.log(document_expedition);
-    console.log(birthday);
-    
-    console.log(gendern);
-    
-    console.log(phone_number);
-    console.log(email);
-    
-    console.log(country);
-    console.log(state);
-    console.log(city);
+
+    let dataUser = {
+        name: nameUser,
+        last_name: last_name,
+        gender: genderUser,
+        birthday: birthdayUser,
+        email: emailUser,
+        phone: phoneNumberUser,
+        document_type : type_document,
+        document: document_number,
+        expedition_date: document_expedition,
+        country: countryUser,
+        state: stateUser,
+        city: cityUser, 
+        password: create_password,
+        created_at: hoy.toLocaleDateString()
+    }
+
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        body: JSON.stringify(dataUser),
+        headers: {'Content-type': 'application/json'}
+    })
+    .then((response) => response.json())
+    .then(data =>{console.log(data);})
     console.log(create_password);
-    console.log(check_password);
-    
-    console.log(termAndConditions);
-    console.log(personalData);
-
 }
-
-
-
-
-
-window.addEventListener('load', (event) => {
-    console.log('The page has fully loaded');
-
-    
-
-
-
-
-
-
-});
-
+btnRegister.onclick = addNewUser
 
